d699489613ed7bb1c23b9af0b94a41600b945888
egor-galkin29 <egorgalkin290907@gmail.com>
2024-05-10 10:20:54 -0400

    makeup cell

--- ImageFeed/Presentation/ImagesList/ImagesListCell.swift
+++ ImageFeed/Presentation/ImagesList/ImagesListCell.swift
@@ -2,5 +2,9 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLable: UILabel!
 }
 
