f7650e40220d27c4e8fcabfedbfdd49c228aec8f
egor-galkin29 <egorgalkin290907@gmail.com>
2024-05-16 09:31:37 -0400

    makeup profile screen

---
+++ ImageFeed/Presentation/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// MARK: - ProfileViewController
+
+final class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var liginNameLable: UILabel!
+    @IBOutlet private var descriptionLable: UILabel!
+    @IBOutlet private var nameLable: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+    // MARK: - Public Methods
+    
+}
