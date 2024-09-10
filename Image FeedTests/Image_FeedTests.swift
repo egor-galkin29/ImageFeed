14a0865fff2bb17baee3a30773339e01cdcfdd8a
egor-galkin29 <egorgalkin290907@gmail.com>
2024-09-10 15:29:29 -0400

    unit test have done

---
+++ Image FeedTests/Image_FeedTests.swift
@@ -0,0 +1,28 @@
+//
+//  Image_FeedTests.swift
+//  Image FeedTests
+//
+//  Created by Егор Галкин on 2024-09-10.
+//
+@testable import ImageFeed
+import XCTest
+
+final class ImagesListServiceTests: XCTestCase {
+    func testFetchPhotos() {
+        let service = ImagesListService.shared
+        
+        let expectation = self.expectation(description: "Wait for Notification")
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(service.photos.count, 10)
+        service.fetchPhotosNextPage()
+    }
+}
